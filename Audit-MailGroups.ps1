80a80df5c51c07b90429d40a43e5909459487472
Roman Zarka <github@zarka.com>
2022-10-04 13:16:38 -0400

    Update Audit-MailGroups.ps1

--- Audit-MailGroups.ps1
+++ Audit-MailGroups.ps1
@@ -1,7 +1,7 @@
 $ScriptInfo = @"
 ================================================================================
 Audit-MailGroups.ps1 | v3.2.3
-by Roman Zarka | Microsoft Services
+by Roman Zarka | github.com/zarkatech
 ================================================================================
 SAMPLE SCRIPT IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND.
 "@; cls; Write-Host "$ScriptInfo`n" -ForegroundColor White
